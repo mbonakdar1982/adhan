38897749d3adbf2427212ff5cc78b64f922547d9
Mohammad Bonakdar <moh.bonakdar@gmail.com>
2023-03-24 08:18:27 -0400

    ramazan program added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import pandas as pd
 import requests
 import funcs
@@ -15,24 +16,39 @@ while True:
     now = datetime.datetime.now()
     todayDate = now.date().strftime("%d-%m-%Y")
     currentTime = now.time().strftime("%H:%M")
+    
+    currentTime_advance = (now + datetime.timedelta(minutes=12, seconds=21)).strftime("%H:%M")
     hour = int(currentTime[0:2])
     hijriDate = prayTimes.loc[todayDate, 'hijriDate']
     [hijri_day, hijri_month, hijri_year] = hijriDate.split('-')
     dhuhrAdhan = prayTimes.loc[todayDate, 'dhuhr']
     maghribAdhan = prayTimes.loc[todayDate, 'maghrib']
     weekday = prayTimes.loc[todayDate, 'weekday']
+    if hijri_month == '09' and currentTime_advance == maghribAdhan:
+        funcs.playdoa(doa='rabbana')
+        funcs.playdoa(doa='tawashih')
+        funcs.playAdhan()
+        funcs.playdoa(doa='ramezanDaily_{}'.format(hijri_day))
+        funcs.playdoa(doa='somna')
+        funcs.playdoa(doa='shahr')
+        funcs.playdoa(doa='eftetah')
+
    
     if currentTime == dhuhrAdhan:
         funcs.playAdhan()
         funcs.playdoa(doa=weekday)
         if hijri_month == '07':
             funcs.playdoa(doa='Rajab')
-        funcs.playdoa()
+        elif hijri_month == '08':
+            funcs.playdoa(doa='shaaban')
+        funcs.playdoa(doa='doa_eftetah' if weekday == 'Wednesday' else 'random')
     if currentTime == maghribAdhan:
         funcs.playAdhan()
         funcs.playdoa(doa=weekday)
         if hijri_month == '07':
             funcs.playdoa(doa='Rajab')
+        elif hijri_month == '08':
+            funcs.playdoa(doa='shaaban')
         funcs.playdoa(doa='doa_komeyl' if weekday == 'Thursday' else 'random')
     
 
@@ -47,6 +63,7 @@ while True:
             funcs.playsound('bell', 4)
         time.sleep(5)
     print(currentTime)
+    print(currentTime_advance)
     print('day: {}, month: {}'.format(hijri_day, hijriMonth[hijri_month]))
     print('Weekday: {}'.format(weekday))
     print('Maghrib: {}'.format(maghribAdhan))
